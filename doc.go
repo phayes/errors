5c04b74acee37c2bab86dbb1bcd89f4f8ec0a426
phayes <patrick.d.hayes@gmail.com>
2014-09-30 14:39:36 -0700

    Improving documentation, adding doc.go

--- doc.go
+++ doc.go
@@ -1,33 +1,25 @@
-phayes/errors: Better error handling for go
------------------------------------------------
-[![GoDoc](https://godoc.org/github.com/phayes/errors?status.svg)](https://godoc.org/github.com/phayes/errors)
-[![Build Status](https://travis-ci.org/phayes/errors.svg?branch=master)](https://travis-ci.org/phayes/errors)
-[![Coverage Status](https://coveralls.io/repos/phayes/errors/badge.png?branch=master)](https://coveralls.io/r/phayes/errors)
-
+/*
 Go's standard `errors` package is a simple error package for handling errors, and works fine for shallow codebases, but has several shortcomings that `phayes/errors`
-hopes to remediate. 
+hopes to remediate.
 
 The biggest anti-pattern that the standard errors package encourages is one we are all familiar with:
- 
-```go
+
     if err != nil
         return errors.New(err.Error() + ". Some more details about the error")
     }
-```
 
-This anti-pattern is corrected in phayes/errors by allowing you to cleanly wrap one error with another like so: 
 
-```go
+This anti-pattern is corrected in phayes/errors by allowing you to cleanly wrap one error with another like so:
+
     if err != nil {
         return errors.Append(err, "Some more details about the error")
     }
-```
 
-This allows us to cleanly add more details to an error, while preseving the underlying error for later inspection. 
+
+This allows us to cleanly add more details to an error, while preseving the underlying error for later inspection.
 
 It also plays nicely with standard library errors and predefined errors
 
-```go
     import (
         "github.com/phayes/errors"
         "io"
@@ -53,8 +45,6 @@ It also plays nicely with standard library errors and predefined errors
             }
         }
     }
-```
-
 
 
 Wrapping errors
@@ -62,13 +52,10 @@ Wrapping errors
 
 At it's most basic, `phayes/errors` is a drop in replacement for the standard error package.
 
-```go
     err := errors.New("Could not parse input")
-```
 
 However, it also provides the ability to wrap an error to give it more context
 
-```go
     import (
         "github.com/phayes/errors"
     )
@@ -87,8 +74,6 @@ However, it also provides the ability to wrap an error to give it more context
     		log.Fatal(err) // Prints "Failed to read stream. unexpected EOF"
     	}
     }
-```
-
 
 
 Inspecting errors
@@ -97,7 +82,6 @@ Inspecting errors
 Use the `IsA` function to check to if the error, or any of it's inner errors, is what you're after. This is fully compatible with errors that
 are not part of phayes/errors. For example:
 
-```go
     func main() {
         var b []byte
         err := ReadStream(b)
@@ -109,5 +93,5 @@ are not part of phayes/errors. For example:
             }
         }
     }
-```
-
+*/
+package errors
